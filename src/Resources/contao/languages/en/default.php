9614120ca8f041002b81991c267578f3a8822d1c
Carsten Götzinger <carsten.goetzinger@concedro.com>
2023-11-06 15:46:52 +0100

    feat: add english translation

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of cgoit\contao-persons-bundle.
+ *
+ * (c) Carsten Götzinger
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+use Cgoit\PersonsBundle\Controller\ContentElement\PersonElement;
+use Cgoit\PersonsBundle\Controller\Module\PersonModule;
+
+$GLOBALS['TL_LANG']['CTE'][PersonElement::TYPE][0] = 'Person';
+$GLOBALS['TL_LANG']['CTE'][PersonElement::TYPE][1] = 'Displays information about a person';
+
+$GLOBALS['TL_LANG']['FMD'][PersonModule::TYPE][0] = 'Person';
+$GLOBALS['TL_LANG']['FMD'][PersonModule::TYPE][1] = 'Displays information about a person';
---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of cgoit\contao-persons-bundle.
+ *
+ * (c) Carsten Götzinger
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+$GLOBALS['TL_LANG']['MOD']['person'][0] = 'Persons';
+$GLOBALS['TL_LANG']['MOD']['person'][1] = 'Manage persons who need to be displayed more frequently in the frontend';
