c0e6b08374b2ca7c3c2cede8b3757051eac7ee7d
cgoIT <info@cgo-it.de>
2024-04-30 12:59:52 +0200

    chore: fix depcheck config

--- depcheck.php
+++ depcheck.php
@@ -7,6 +7,8 @@ use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 $config = new Configuration();
 $config
+    ->ignoreErrorsOnPackage('contao/image', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/translation-contracts', [ErrorType::SHADOW_DEPENDENCY])
     ->ignoreErrorsOnPackage('codefog/contao-haste', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('mvo/contao-group-widget', [ErrorType::UNUSED_DEPENDENCY]);
 
